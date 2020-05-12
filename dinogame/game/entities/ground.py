b270923f8154635ea67ed5997e219ea9b99c9701
Roberto Bochet <robertobochet@gmail.com>
2020-05-12 23:46:56 +0200

    Add ground entity

---
+++ dinogame/game/entities/ground.py
@@ -0,0 +1,38 @@
+import os
+
+import pygame
+
+GROUND_FILE = "../../assets/ground.jpg"
+
+
+class Ground(pygame.sprite.Group):
+    def __init__(self, ground_position: float):
+        super(Ground, self).__init__()
+
+        self.add(GroundPiece(ground_position, 0, 2))
+        self.add(GroundPiece(ground_position, 1, 2))
+
+    def update(self, dx: float = 5.0):
+        super(Ground, self).update(dx)
+
+
+class GroundPiece(pygame.sprite.Sprite):
+    def __init__(self, ground_position: float, piece: int, pieces: int):
+        super(GroundPiece, self).__init__()
+
+        sprite = pygame.image.load(os.path.join(os.path.dirname(__file__), GROUND_FILE))
+        _, _, w, h = sprite.get_rect()
+
+        self._dw = w
+
+        self.image = sprite.subsurface((piece * w / pieces, 0, w / pieces, h)).convert()
+        self.rect = self.image.get_rect()
+
+        self.rect.top = ground_position
+        self.rect.left = piece * w / pieces
+
+    def update(self, dx: float):
+        self.rect.x -= dx
+
+        if self.rect.right < 0:
+            self.rect.left += self._dw
