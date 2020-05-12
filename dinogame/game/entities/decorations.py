c9151959eb60a02ea9a04feabd26c67a94868183
Roberto Bochet <robertobochet@gmail.com>
2020-05-13 01:20:33 +0200

    Add Decorations entities

---
+++ dinogame/game/entities/decorations.py
@@ -0,0 +1,47 @@
+import os
+import random
+from typing import Tuple
+
+import pygame
+
+CLOUD_FILE = "../../assets/cloud.png"
+COLOR_KEY = (0, 0, 0)
+
+
+class Decorations(pygame.sprite.Group):
+    def __init__(self,
+                 stage_width: float,
+                 clouds_vertical_position: Tuple[float, float],
+                 cloud_spawn_rate: float = 0.02):
+        super(Decorations, self).__init__()
+
+        self._stage_width = stage_width
+
+        self._clouds_y = clouds_vertical_position
+
+        self._cloud_spawn_rate = cloud_spawn_rate
+
+    def update(self):
+        if random.random() < self._cloud_spawn_rate:
+            self.add(Cloud((self._stage_width + 20, random.uniform(*self._clouds_y))))
+
+        super(Decorations, self).update()
+
+
+class Cloud(pygame.sprite.Sprite):
+    def __init__(self, position: Tuple[float, float]):
+        super(Cloud, self).__init__()
+
+        self.image = pygame.image.load(os.path.join(os.path.dirname(__file__), CLOUD_FILE)).convert()
+
+        self.image.set_colorkey(COLOR_KEY)
+
+        self.rect = self.image.get_rect()
+
+        self.rect.center = position
+
+    def update(self, dx: float = 0.03):
+        self.rect.x -= dx
+
+        if self.rect.right < 0:
+            self.kill()
