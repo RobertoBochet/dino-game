4e79db102ad32a33a9d2cc369de0fd709bccf385
Roberto Bochet <robertobochet@gmail.com>
2020-05-13 22:16:41 +0200

    Add README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dino Game
+
+A Python reimplementation of the famous dino game, thought for autonomous control
--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
-from distutils.core import setup
+from setuptools import setup
+
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='dino-game',
     packages=['dinogame'],
-    version='0.1',
+    version='0.1a1',
     license='gpl-3.0',
     description='A Python reimplementation of the famous dino game, thought for autonomous control',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Roberto Bochet',
     author_email='robertobochet@gmail.com',
     url='https://github.com/RobertoBochet/dino-game',
@@ -14,6 +22,7 @@ setup(
         'pygame>=2.0.0.dev6',
         'numpy'
     ],
+    package_data={'dinogame': ['assets/*.png']},
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
